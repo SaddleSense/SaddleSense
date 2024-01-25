3409a1101bbd0dec52200ebdb698252eb9ab91b9
arty <artyblouin@gmail.com>
2024-01-24 23:55:34 -0800

    updated images and gif on loading screen

--- app.js
+++ app.js
@@ -8,14 +8,13 @@ const questions = [
        answers: [
            {
                text: "Male",
-            //    image: "https://images.unsplash.com/photo-1534430480872-3498386e7856?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjczMTc0fQ&fit=crop&h=230&w=320&crop=edges",
                image: "img/male.png",
                alt: "Photo of the Empire State building during daytime",
                credit: "Oliver Niblett"
            },
            {
                text: "Female",
-               image: "https://images.unsplash.com/photo-1531218150217-54595bc2b934?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjczMTc0fQ&fit=crop&h=230&w=320&crop=edges",
+               image: "img/female.png",
                alt:"Time-lapse photography car lights on bridge",
                credit: "Carlos Alfonso"
            }
@@ -23,29 +22,29 @@ const questions = [
     },
     {
         id: 1,
-        text: "Preferred riding style?",
+        text: "Preferred cycling style?",
         answers: [
             {
-                text: "Pizza",
-                image: "https://images.unsplash.com/photo-1534308983496-4fabb1a015ee?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjczMTc0fQ&fit=crop&h=230&w=320&crop=edges",
+                text: "Gravel",
+                image: "img/gravel_cycling.jpg",
                 alt:"Pepperoni Pizza",
                 credit: "Alan Hardman"
             },
             {
-                text: "Sandwich",
-                image: "https://images.unsplash.com/photo-1481070414801-51fd732d7184?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjczMTc0fQ&fit=crop&h=230&w=320&crop=edges",
+                text: "Mountain",
+                image: "img/mountain_cycling.jpg",
                 alt:"ham sandwich on white surface",
                 credit: "Eaters Collective"
             },
             {
-                text: "Pasta",
-                image: "https://images.unsplash.com/photo-1516100882582-96c3a05fe590?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjczMTc0fQ&fit=crop&h=230&w=320&crop=edges",
+                text: "Road",
+                image: "img/road_cycling.jpg",
                 alt:"Pasta in tomato sauce",
                 credit: "Mgg Vitchakorn"
             },
             {
-                text: "Hamburger",
-                image: "https://images.unsplash.com/photo-1551782450-a2132b4ba21d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjczMTc0fQ&fit=crop&h=230&w=320&crop=edges",
+                text: "Commuter",
+                image: "img/commuter_cycling.jpg",
                 alt:"hamburger",
                 credit: "sk"
             }
@@ -54,29 +53,35 @@ const questions = [
     },
     {
         id: 2,
-        text: "Preferred riding position?",
+        text: "Preferred cycling position?",
         answers: [
             {
-                text: "Traditional",
-                image: "https://images.unsplash.com/photo-1555040479-c949debe66c1?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjczMTc0fQ&fit=crop&h=230&w=320&crop=edges",
+                text: "Leisure",
+                image: "img/leisure.png",
                 alt:"focus photography of building windows",
                 credit: "Burgess Milner"
             },
             {
-                text: "Modern",
-                image: "https://images.unsplash.com/photo-1460317442991-0ec209397118?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjczMTc0fQ&fit=crop&h=230&w=320&crop=edges",
+                text: "Fitness",
+                image: "img/fitness.png",
                 alt:"low angle view of building",
                 credit: "Brandon Giggs"
             },
             {
-                text: "House",
-                image: "https://images.unsplash.com/photo-1572120360610-d971b9d7767c?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjczMTc0fQ&fit=crop&h=230&w=320&crop=edges",
+                text: "Performance",
+                image: "img/performance.png",
                 alt:"trees beside white house",
                 credit: "Phil Hearing"
             },
             {
-                text: "Mountains",
-                image: "https://images.unsplash.com/photo-1506974210756-8e1b8985d348?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjczMTc0fQ&fit=crop&h=230&w=320&crop=edges",
+                text: "Aggressive",
+                image: "img/aggressive.png",
+                alt:"trees beside white house",
+                credit: "Phil Hearing"
+            },
+            {
+                text: "Aerodynamic",
+                image: "img/aerodynamic.png",
                 alt:"brown wooden cabin infront of forest",
                 credit: "eulauretta"
             }
